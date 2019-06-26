c0ea09e10c55278bc6243ac3c9ea5e1cd5473a51
Rafał <heller.rafal@gmail.com>
2019-06-26 16:07:41 +0200

    adding global styles

---
+++ src/theme/GlobalStyle.js
@@ -0,0 +1,20 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+@import url('https://fonts.googleapis.com/css?family=Montserrat:300,600&display=swap&subset=latin-ext');
+
+*,*::before, *::after {
+box-sizing: border-box;
+}
+
+html {
+font-size: 62.5%;
+}
+
+body {
+font-family: "Montserrat", sans-serif, Arial;
+font-size: 1.6rem;
+}
+
+`;
+export default GlobalStyle;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import Button from 'components/Button/Button';
+import GlobalStyle from 'theme/GlobalStyle';
 
 const Root = () => (
   <div>
+    <GlobalStyle />
     <h1>hello</h1>
     <Button>Close / save</Button>
     <Button secondary>Remove</Button>
