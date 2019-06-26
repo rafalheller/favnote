669248b34b24cb7e987b1dd1dc69287abd1988ea
Rafał <heller.rafal@gmail.com>
2019-06-26 15:51:32 +0200

    add Button component with three styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,6 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 const Button = styled.button`
+  padding: 0;
   background-color: #ffd82b;
+  width: 220px;
+  height: 47px;
+  border: none;
+  border-radius: 50px;
+  font-family: 'Montserrat', 'Arial';
+  font-weight: 500;
+  font-size: 16px;
+  text-transform: uppercase;
+
+  ${({ secondary }) =>
+    secondary &&
+    css`
+      width: 105px;
+      height: 30px;
+      font-size: 10px;
+      background-color: #e6e6e6;
+    `}
+  ${({ active }) =>
+    active &&
+    css`
+      border: 3px solid black;
+    `}
 `;
 export default Button;
--- src/views/Root/Root.js
+++ src/views/Root/Root.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import Button from 'components/Button/Button';
 
 const Root = () => (
   <div>
     <h1>hello</h1>
+    <Button>Close / save</Button>
+    <Button secondary>Remove</Button>
+    <Button active>Remove</Button>
   </div>
 );
 
